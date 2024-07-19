a67911d765fea543e35f61b117444f44949fb563
05JesusMoreno <jesusbautistam845@gmail.com>
2024-07-19 14:12:11 -0600

    Subo cambios en controlador para productos, empleados, rutas,

--- src/routes/Empleados.routes.js
+++ src/routes/Empleados.routes.js
@@ -6,5 +6,9 @@ const authController = require('../controllers/empleado.controller');
 router.get('/empleado', authController.getAllEmpleado); // Utilizar el método login del controlador de autenticación
 router.post('/crear', authController.createEmpleado); // Utilizar el método login del controlador de autenticación
 router.post('/login', authController.loginEmpleado); // Utilizar el método login del controlador de autenticación
+router.put("/empleado/:id", authController.updateEmpleado);
+router.get('/empleado/:id', authController.getEmpleadoById);
+router.patch("/empleado/desactivar/:id", authController.deactivateEmpleado);
+router.put('/empleado/:id/baja', authController.deactivateEmpleado);
 
 module.exports = router;
