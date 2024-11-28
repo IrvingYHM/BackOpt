2777410061e0981807709a6415d66898cdb28639
05JesusMoreno <124802542+05JesusMoreno@users.noreply.github.com>
2024-11-28 09:59:02 -0600

    Update feedbackMovil.routes.js

--- src/routes/feedbackMovil.routes.js
+++ src/routes/feedbackMovil.routes.js
@@ -6,7 +6,13 @@ const Feedback = require("../controllers/feedbackMovil.controller");
 // Ruta para obtener todas las direcciones de los clientes
 router.post("/EncuestaM", Feedback.crearEncuesta);
 router.get("/EncuestaM/completada", Feedback.verificarEncuesta);
+router.post("/guardarEncuestaPendientes", Feedback.guardarEncuestaPendiente);
 router.get("/resultadosM", Feedback.obtenerResultadosEncuestas);
+router.put("/RegistroE", Feedback.completarEncuesta);
+router.post("/verificaE", Feedback.registrarAccesoFeedback)
+router.post("/obtenerEncuestaPendiente", Feedback.obtenerEncuestaPendiente);
+
+
 
 
 
